4f755b554ce0a4ab42107c56362eadfd85ed4f12
Eliott-rjd <eliott.rajaud@gmail.com>
2023-12-06 12:09:27 +0100

    reverse with real routes and better bash

--- reverse-proxy.sh
+++ reverse-proxy.sh
@@ -3,5 +3,5 @@
 
 cp ./nginx.conf /tmp/
 
-sudo docker run --name asi2-nginx-container --network host -v /tmp/nginx.conf:/etc/nginx/nginx.conf:ro -d nginx
+sudo docker run --name asi2-nginx-container --network host -v /tmp/nginx.conf:/etc/nginx/nginx.conf:ro --rm nginx
 
