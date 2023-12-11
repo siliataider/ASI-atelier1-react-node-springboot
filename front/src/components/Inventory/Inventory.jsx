2929ca19cbdb8cf17f1ecf474383b1e130d01afb
Eliott-rjd <eliott.rajaud@gmail.com>
2023-12-11 14:17:36 +0100

    api to get cards_to_sell and sell them

--- front/src/components/Inventory/Inventory.jsx
+++ front/src/components/Inventory/Inventory.jsx
@@ -6,26 +6,64 @@ import Card from '../Card/Card';
 const Inventory = () => {
 
   const [userCards, setUserCards] = useState([]);
-  const cards = useSelector((state) => state.shop.cards);//TODO: remove when backend connexion
+  const currentUserId = useSelector((state) => state.auth.currentUserId);
+
+  const getUserCards = async () => {
+    try {
+      const response = await fetch(`${config.BASE_URL}/cards_to_sell`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch user cards');
+      }
+      const jsonUserCards = await response.json();
+      console.log(jsonUserCards)
+      setUserCards(jsonUserCards);
+    } catch (error) {
+      console.error('Erreur lors de la récupération des données GET:', error);
+    }
+  };
 
   useEffect(() => {
-    const getUserCards = async () => {
-      try {
-        const response = await fetch('http://localhost:80/cards');
-        if (!response.ok) {
-          throw new Error('Failed to fetch user cards');
-        }
-        const jsonUserCards = await response.json();
-        console.log(jsonUserCards)
-        setUserCards(jsonUserCards);
-      } catch (error) {
-        console.error('Erreur lors de la récupération des données GET:', error);
-      }
-    };
-
     getUserCards()
   }, []); // Le tableau vide signifie que useEffect ne s'exécutera qu'une fois, équivalent à componentDidMount
-
+  
+  const handleSellClick = async (currentCard) => {
+  
+    if (!currentCard.id || !currentUserId) {
+      alert('No card selected or user ID not found!');
+      return;
+    }
+  
+    const order = {
+      user_id: parseInt(currentUserId),
+      card_id: parseInt(currentCard.id)
+    };
+  
+    try {
+      const response = await fetch(`${config.BASE_URL}/sell`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(order),
+      });
+  
+      if (!response.ok) {
+        throw new Error('Failed to sell card');
+      }
+  
+      const purchaseSuccess = await response.json();
+      if (purchaseSuccess) {
+        await getUserCards();
+        alert(`Successfully sell with ID: ${currentCard.id}`);
+      } else {
+        alert('Failed to sell card. Please try again.');
+      }
+  
+    } catch (error) {
+      console.error('Error during sell card request:', error);
+      alert('Failed to sell card');
+    }
+  };  
 
   return (
     <div>
@@ -33,7 +71,7 @@ const Inventory = () => {
       {userCards.map((card) => (
         <div key={card.id}>
           <Card card = {card} showCardDetails = {true} />
-          <button className='btn btn-info pb-1'>Sell</button>
+          <button className='btn btn-info pb-1' onClick={() => handleSellClick(card)} >Sell</button>
         </div>
       ))}
     </div>
